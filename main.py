35a5f76db3bbc2c02b899243637cd786d402d8e3
Grant01~ <awesomedude2128@gmail.com>
2023-12-06 19:07:58 -0600

    finished legal moves

--- main.py
+++ main.py
@@ -21,9 +21,15 @@ class checker:
             s+= '\n'
         return s
 class game:
+
+    #FOR METHOD BELOW
+    #probably should change the black and white circles and just represent the types of colors as numbers-probably way faster
     @staticmethod
     def legalMoves(board,x,y,color):
         moves = []
+        
+        #some statement here to handle if it is not a checker object
+
 
         # if(x-1 >= 0 and y-1 >= 0 and (board[x+1][y+1] == '⚪' and board[x+1][y+1] != '⚫')):
         #     moves.append([x+1,y+1])
@@ -33,14 +39,61 @@ class game:
         #     moves.append([x+1,y+1])
 
         if(color == 'white'):
-            if(x-1 >= 0 and y-1 >= 0 and board[x-1][y-1] != '⚪' and board[x-1][y-1] != '⚫'):
+            if(x-1 >= 0 and y-1 >= 0 and str(board[x-1][y-1]) != '⚪' and str(board[x-1][y-1]) != '⚫'):
                 moves.append([x-1,y-1])
-            if(x-1 >= 0 and y+1 <= 7 and board[x-1][y+1] != '⚪' and board[x-1][y+1] != '⚫'):
+            if(x-1 >= 0 and y+1 <= 7 and str(board[x-1][y+1]) != '⚪' and str(board[x-1][y+1]) != '⚫'):
                 moves.append([x-1,y+1])
-            if(x+2 <= 7 and y+2 <= 7 and board[x+1][y+1] == '⚫' and (board[x+2][y+2] != '⚫' and board[x+2][y+2] != '⚪')):
-               moves.append(['capture',x+2,y+2])
-            if(x-2 >= 0 and y+2 <= 7 and board[x-1][y+1] == '⚫' and (board[x-2][y+2] != '⚫' and board[x-2][y+2] != '⚪')):
-               moves.append(['capture',x-2,y+2])
+            if(x-2 >= 0 and y-2 >= 0 and str(board[x-1][y-1]) == '⚫' and (str(board[x-2][y-2]) != '⚫' and str(board[x-2][y-2]) != '⚪')):
+               moves.append(['capture',[x-1,y-1],x-2,y-2])
+            if(x-2 >= 0 and y+2 <= 7 and str(board[x-1][y+1]) == '⚫' and (str(board[x-2][y+2]) != '⚫' and str(board[x-2][y+2]) != '⚪')):
+               moves.append(['capture',[x-1,y+1],x-2,y+2])
+        
+        if(color == 'black'):
+            if(x+1 <= 7 and y+1 <= 7 and str(board[x+1][y+1]) != '⚪' and str(board[x+1][y+1]) != '⚫'):
+                moves.append([x+1,y+1])
+            if(x+1 <= 7 and y-1 >= 0 and str(board[x+1][y-1]) != '⚪' and str(board[x+1][y-1]) != '⚫'):
+                moves.append([x+1,y-1])
+            if(x+2 <= 7 and y-2 >= 0 and str(board[x+1][y-1]) == '⚪' and (str(board[x+2][y-2]) != '⚫' and str(board[x+2][y-2]) != '⚪')):
+               moves.append(['capture',[x+1,y-1],x+2,y-2])
+            if(x+2 <= 7 and y+2 <= 7 and str(board[x+1][y+1]) == '⚪' and (str(board[x+2][y+2]) != '⚫' and str(board[x+2][y+2]) != '⚪')):
+               moves.append(['capture',[x+1,y+1],x+2,y+2])
+
+        if(color == 'blackKing'):
+            if(x+1 <= 7 and y+1 <= 7 and str(board[x+1][y+1]) != '⚪' and str(board[x+1][y+1]) != '⚫'):
+                moves.append([x+1,y+1])
+            if(x+1 <= 7 and y-1 >= 0 and str(board[x+1][y-1]) != '⚪' and str(board[x+1][y-1]) != '⚫'):
+                moves.append([x+1,y-1])
+            if(x+2 <= 7 and y-2 >= 0 and str(board[x+1][y-1]) == '⚪' and (str(board[x+2][y-2]) != '⚫' and str(board[x+2][y-2]) != '⚪')):
+               moves.append(['capture',[x+1,y-1],x+2,y-2])
+            if(x+2 <= 7 and y+2 <= 7 and str(board[x+1][y+1]) == '⚪' and (str(board[x+2][y+2]) != '⚫' and str(board[x+2][y+2]) != '⚪')):
+               moves.append(['capture',[x+1,y+1],x+2,y+2])
+            if(x-1 >= 0 and y-1 >= 0 and str(board[x-1][y-1]) != '⚪' and str(board[x-1][y-1]) != '⚫'):
+                moves.append([x-1,y-1])
+            if(x-1 >= 0 and y+1 <= 7 and str(board[x-1][y+1]) != '⚪' and str(board[x-1][y+1]) != '⚫'):
+                moves.append([x-1,y+1])
+            if(x-2 >= 0 and y-2 >= 0 and str(board[x-1][y-1]) == '⚪' and (str(board[x-2][y-2]) != '⚫' and str(board[x-2][y-2]) != '⚪')):
+               moves.append(['capture',[x-1,y-1],x-2,y-2])
+            if(x-2 >= 0 and y+2 <= 7 and str(board[x-1][y+1]) == '⚪' and (str(board[x-2][y+2]) != '⚫' and str(board[x-2][y+2]) != '⚪')):
+               moves.append(['capture',[x-1,y+1],x-2,y+2])
+
+        if(color == 'whiteKing'):
+            if(x+1 <= 7 and y+1 <= 7 and str(board[x+1][y+1]) != '⚪' and str(board[x+1][y+1]) != '⚫'):
+                moves.append([x+1,y+1])
+            if(x+1 <= 7 and y-1 >= 0 and str(board[x+1][y-1]) != '⚪' and str(board[x+1][y-1]) != '⚫'):
+                moves.append([x+1,y-1])
+            if(x+2 <= 7 and y-2 >= 0 and str(board[x+1][y-1]) == '⚫' and (str(board[x+2][y-2]) != '⚫' and str(board[x+2][y-2]) != '⚪')):
+               moves.append(['capture',[x+1,y-1],x+2,y-2])
+            if(x+2 <= 7 and y+2 <= 7 and str(board[x+1][y+1]) == '⚫' and (str(board[x+2][y+2]) != '⚫' and str(board[x+2][y+2]) != '⚪')):
+               moves.append(['capture',[x+1,y+1],x+2,y+2])
+            if(x-1 >= 0 and y-1 >= 0 and str(board[x-1][y-1]) != '⚪' and str(board[x-1][y-1]) != '⚫'):
+                moves.append([x-1,y-1])
+            if(x-1 >= 0 and y+1 <= 7 and str(board[x-1][y+1]) != '⚪' and str(board[x-1][y+1]) != '⚫'):
+                moves.append([x-1,y+1])
+            if(x-2 >= 0 and y-2 >= 0 and str(board[x-1][y-1]) == '⚫' and (str(board[x-2][y-2]) != '⚫' and str(board[x-2][y-2]) != '⚪')):
+               moves.append(['capture',[x-1,y-1],x-2,y-2])
+            if(x-2 >= 0 and y+2 <= 7 and str(board[x-1][y+1]) == '⚫' and (str(board[x-2][y+2]) != '⚫' and str(board[x-2][y+2]) != '⚪')):
+               moves.append(['capture',[x-1,y+1],x-2,y+2])
+
         
         return moves
 
@@ -55,7 +108,10 @@ for i in range(0,2):
 for i in range(6,8):
     for j in range(0,8):
         board[i][j] = checker('white',0,0)
+board[4][5] = board[0][0]
+board[3][6] = board[7][7]
 print(checker.toString(board))
 s = type(1)
-print(game.legalMoves(board,6,7,'white'))
-print(board[5])
+
+print(game.legalMoves(board,4,5,'blackKing'))
+
